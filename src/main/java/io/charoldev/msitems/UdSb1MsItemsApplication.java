ecd032c63a982a3cd823caee5c663083d4ea93cb
charoldev <harolrop14@gmail.com>
2024-10-22 14:19:15 -0500

    first commit

---
+++ src/main/java/io/charoldev/msitems/UdSb1MsItemsApplication.java
@@ -0,0 +1,13 @@
+package io.charoldev.msitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdSb1MsItemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdSb1MsItemsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/charoldev/msitems/UdSb1MsItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.charoldev.msitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdSb1MsItemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
