cc312e90dfd55c5b1e7044f02b541b30d2dd5ef7
charoldev <harolrop14@gmail.com>
2024-10-22 19:14:22 -0500

    item controller

---
+++ src/main/java/io/charoldev/msitems/controller/ItemController.java
@@ -0,0 +1,32 @@
+package io.charoldev.msitems.controller;
+
+import io.charoldev.msitems.model.ItemDto;
+import io.charoldev.msitems.service.ItemServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemServiceImpl itemService;
+
+    @GetMapping
+    public List<ItemDto> list() {
+        return itemService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemDto> details(@PathVariable Long id) {
+        Optional <ItemDto> itemOptional = itemService.findById(id);
+        return itemOptional
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
